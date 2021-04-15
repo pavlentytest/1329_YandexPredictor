f53aee8cf24a5e23b91852dce657263e794ef0b9
Pavel <impetus@yandex.ru>
2021-04-15 15:56:26 +0300

    Initial commit

---
+++ app/src/main/java/ru/samsung/itschool/mdev/myapplication/Model.java
@@ -0,0 +1,7 @@
+package ru.samsung.itschool.mdev.myapplication;
+
+public class Model {
+    public boolean endOfWord;
+    public int pos;
+    public String[] text;
+}
---
+++ app/src/main/java/ru/samsung/itschool/mdev/myapplication/YandexAPI.java
@@ -0,0 +1,10 @@
+package ru.samsung.itschool.mdev.myapplication;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface YandexAPI {
+    @GET("api/v1/predict.json/complete")
+    Call<Model> predict(@Query("key") String key, @Query("q") String q, @Query("lang") String lang);
+}
